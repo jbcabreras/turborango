bf0f560bdf520aef27092924f41b3226031fb6f8
Rodrigo Frohlich <rodrigofcb@gmail.com>
2015-06-02 11:28:16 -0300

    exercícios Turbo Rango 1A, 1B e 1C

---
+++ src/Solution/TurboRango.Dominio/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    internal enum Categoria
+    {
+        COMUM,
+        COZINHANATURAL,
+        COZINHAMEXICANA,
+        CHURRASCARIA,
+        COZINHAJAPONESA,
+        FASTFOOD,
+        PIZZARIA
+    }
+}
---
+++ src/Solution/TurboRango.Dominio/Restaurante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurboRango.Dominio
+{
+    internal class Restaurante
+    {
+        internal int Capacidade { get; set; }
+        internal string Nome { get; set; }
+        internal Localizacao Localizacao;
+        internal Contato Contato;
+        internal Categoria Tipo;
+
+    }
+}
